b28b006895dd262cbe55d6c2f17d99b7c05223e1
Del-985 <119140366+Del-985@users.noreply.github.com>
2025-08-10 19:26:37 -0500

    Create slots.hpp

---
+++ include/slots.hpp
@@ -0,0 +1,21 @@
+#pragma once
+namespace game {
+
+enum class Slot { Weapon, Offhand, Armor, Helmet, Boots, Belt, Amulet, Ring1, Ring2 };
+
+inline const char* slotName(Slot s) {
+    switch (s) {
+        case Slot::Weapon:  return "Weapon";
+        case Slot::Offhand: return "Off-hand";
+        case Slot::Armor:   return "Armor";
+        case Slot::Helmet:  return "Helmet";
+        case Slot::Boots:   return "Boots";
+        case Slot::Belt:    return "Belt";
+        case Slot::Amulet:  return "Amulet";
+        case Slot::Ring1:   return "Ring 1";
+        case Slot::Ring2:   return "Ring 2";
+    }
+    return "?";
+}
+
+} // namespace game
