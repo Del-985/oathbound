9d25c5aeeba613b55869e649c64c49ac4e6e1c0f
Del-985 <119140366+Del-985@users.noreply.github.com>
2025-08-21 14:09:13 -0500

    Update rarity.hpp

--- include/game/rarity.hpp
+++ include/game/rarity.hpp
@@ -1,19 +1,14 @@
 #pragma once
 #include <string>
-
 namespace game {
-
 enum class Rarity { Common, Magic, Rare, Epic, Legendary };
-
 inline std::string rarityName(Rarity r) {
     switch(r){
-        case Rarity::Common:    return "Common";
-        case Rarity::Magic:     return "Magic";
-        case Rarity::Rare:      return "Rare";
-        case Rarity::Epic:      return "Epic";
+        case Rarity::Common: return "Common";
+        case Rarity::Magic: return "Magic";
+        case Rarity::Rare: return "Rare";
+        case Rarity::Epic: return "Epic";
         case Rarity::Legendary: return "Legendary";
-    }
-    return "Common";
+    } return "Common";
 }
-
 } // namespace game
